b9a7557ba4950172a0af517860bd9058148399d2
cutestricly <ayfayfaaa@gmail.com>
2026-05-16 15:08:39 +0700

    last

--- login/public/script.js
+++ login/public/script.js
@@ -1,22 +1,20 @@
-const API_URL = "https://glossid.up.railway.app";
-
 async function login() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-  const res = await fetch(`${API_URL}/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ username, password })
-  });
+    const res = await fetch("https://glossid.up.railway.app/api/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password })
+    });
 
-  const data = await res.json();
+    const data = await res.json();
 
-  if (data.success) {
-    window.location.href = "/dashboard.html";
-  } else {
-    document.getElementById("msg").innerText = data.message;
-  }
+    if (data.success) {
+        window.location.href = "dashboard.html";
+    } else {
+        document.getElementById("msg").innerText = data.message;
+    }
 }
