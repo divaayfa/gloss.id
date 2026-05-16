6ba0ddd6c07d6f531e6fb4da92cb552adbffc02e
cutestricly <ayfayfaaa@gmail.com>
2026-05-16 09:14:24 +0700

    fix i

--- login/backend/index.js
+++ login/backend/index.js
@@ -1,17 +1,26 @@
-const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-// serve frontend
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
+app.use(express.json());
 
+const authRoutes = require('./routes/auth');
+
+// route utama
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.send('Server jalan!');
 });
+
+// route login
+app.use('/api', authRoutes);
+
+const reportRoutes = require('./routes/report');
+app.use('/api', reportRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server jalan di port", PORT);
+  console.log(`Server jalan di port ${PORT}`);
 });
